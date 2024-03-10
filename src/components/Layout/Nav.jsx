f82e1bce7dad21fb002b0c76b74695682f3a6a10
Mariya <mariya@MacBook-Air-Mariya.local>
2024-03-10 00:21:34 -0500

    add logo

--- src/components/Layout/Nav.jsx
+++ src/components/Layout/Nav.jsx
@@ -4,16 +4,17 @@ import { Link } from 'react-router-dom';
 const Nav = ({ isLoggedIn }) => {
   return (
     <nav>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/projects">Browse Projects</Link></li>
-      {!isLoggedIn && 
+      <img src="./images/logo.svg" alt="logo" />
+      <ul>
+        <li><Link to="/">DevConnection</Link></li>
+        <li><Link to="/projects">Browse Projects</Link></li>
+        {!isLoggedIn && 
         <li>
           <Link to="/register">Join Now</Link>
         </li>}
         {!isLoggedIn ? (
           <li><Link to="/login">Sign In</Link></li>
-        ) : (
+          ) : (
           <>
             <li><Link to="/messaging">Messaging</Link></li>
             <li><Link to="/notification">Notification</Link></li>
