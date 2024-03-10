979701cd7fe89bdd783af425921e0613befd77eb
Mariya <mariya@MacBook-Air-Mariya.local>
2024-03-09 21:28:36 -0500

    add project description. add basic design.

--- src/components/Layout/LandingPage.jsx
+++ src/components/Layout/LandingPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Layout.css';
+
 
 const LandingPage = () => (
-    <div>
-      <h1>This is home/welcome page.</h1>
+  <div class="container">
+    <div class="box" id="box1">
+      <h3><strong>DevConnection:</strong> Personal Journey into the World of Professional Development</h3>
+      <h5>Welcome to DevConnection—a platform created specifically for aspiring developers, web designers, and testers, among others. Here, at the intersection of technology and creativity, you embark on your exciting journey into professional development.</h5>
     </div>
-  );
+    <div class="box" id="box2">
+    </div>    
+  </div>
+);
 
-  export default LandingPage;
+export default LandingPage;
