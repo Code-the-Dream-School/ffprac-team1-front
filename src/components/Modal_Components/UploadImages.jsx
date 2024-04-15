ec885f660215e88d22f5f613aed1c3239c37f463
mariyador <aniknorod@gmail.com>
2024-04-14 23:59:12 -0400

    Image sends to cloudinary

--- src/components/Modal_Components/UploadImages.jsx
+++ src/components/Modal_Components/UploadImages.jsx
@@ -3,74 +3,42 @@ import axios from 'axios';
 
 const UploadImage = ({ projectId }) => {
   const [file, setFile] = useState();
-  const [coverProjectPicture, setCoverProjectPicture] = useState();
+  
 
   function handleChange(e) {
-    const imageFile = e.target.files[0];
-    setFile(imageFile);
+    const image = e.target.files[0];
+    setFile(image);
   }
 
-  function handleCoverPictureChange(e) {
-    const coverImageFile = e.target.files[0];
-    setCoverProjectPicture(coverImageFile);
-  }
 
   function handleSubmit() {
     const formData = new FormData();
-    if (file) {
-      formData.append('projectPicture', file);
-      axios
-        .patch(`http://localhost:8000/api/v1/projects/${projectId}`, formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-          withCredentials: true,
-        })
-        .then(response => {
-          console.log(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    } else {
-      console.error('No file selected');
-    }
+    formData.append('projectPicture', file);
+    axios
+      .patch(`http://localhost:8000/api/v1/projects/${projectId}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        withCredentials: true,
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
   return (
     <>
       <header className="text-center text-xl pb-6 font-bold text-green"> Upload Image</header>
-      <div>
-        <label htmlFor="projectPicture">Project Picture:</label>
-        <input
-          type="file"
-          id="projectPicture"
-          name="projectPicture"
-          onChange={handleChange}
-          className="hover:cursor-pointer"
-        />
-      </div>
-      <div>
-        <label htmlFor="coverProjectPicture">Cover Project Picture:</label>
-        <input
-          type="file"
-          id="coverProjectPicture"
-          name="coverProjectPicture"
-          onChange={handleCoverPictureChange}
-          className="hover:cursor-pointer"
-        />
-      </div>
+      <input type="file" onChange={e => handleChange(e)} className="hover:cursor-pointer" />
       <div className="content-center">
         <header className="text-center text-ld pb-6 font-bold text-gray"> Image preview</header>
         <img
           src={file ? URL.createObjectURL(file) : ''}
           className="h-56 w-56 rounded-full object-cover object-center bg-black"
           alt="Preview"
-        />
-        <img
-          src={coverProjectPicture ? URL.createObjectURL(coverProjectPicture) : ''}
-          className="h-56 w-56 rounded-full object-cover object-center bg-black"
-          alt="Cover Preview"
         />
       </div>
       <button
