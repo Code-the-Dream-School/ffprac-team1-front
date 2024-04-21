f12857ec3cfeb971df0f3cf6844ad3e9de633c88
mariyador <aniknorod@gmail.com>
2024-04-20 22:20:55 -0400

    add ba kend technologies

--- src/components/Modal_Components/EditProject.jsx
+++ src/components/Modal_Components/EditProject.jsx
@@ -1,62 +1,132 @@
-import React, { useState } from "react";
-import { Input, Textarea } from "@material-tailwind/react";
+import React, { useState } from 'react';
+import { Input, Textarea } from '@material-tailwind/react';
 import { XCircleIcon } from '@heroicons/react/24/outline';
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-
-const EditProject = ({ projectId, projectTitle: initialProjectTitle, projectDesc: initialProjectDesc, projectRolesNeeded: initialProjectRolesNeeded }) => {
-  const navigate = useNavigate()
+const EditProject = ({
+  projectId,
+  projectTitle: initialProjectTitle,
+  projectDesc: initialProjectDesc,
+  projectRolesNeeded: initialProjectRolesNeeded,
+}) => {
+  const navigate = useNavigate();
 
   const [projectTitle, setProjectTitle] = useState(initialProjectTitle);
   const [projectDesc, setProjectDesc] = useState(initialProjectDesc);
   const [projectRolesNeeded, setProjectRolesNeeded] = useState(initialProjectRolesNeeded);
-  
-  const [frontEnd, setFrontEnd] = useState([]);
-  const [selectedFrontEnd, setSelectedFrontEnd] = useState("");
-  const [frontEndList, setFrontEndList] = useState([" ", "HTML/CSS", "JavaScript", "TypeScript", "React", "Angular", "Vue.js", "Svelte", "Next.js", "Redux", "Bootstrap", "Tailwind CSS", "SASS/LESS"]);
+  const [selectedRoles, setSelectedRoles] = useState(initialProjectRolesNeeded);
 
-  const handleChange = (e) => {
+  const allRoles = [
+    'Mentor',
+    'Frontend Developer',
+    'Backend Developer',
+    'Fullstack Developer',
+    'Team Lead',
+    'UI/UX Designer',
+    'Project Manager',
+    'DevOps Engineer',
+    'Quality Assurance Engineer',
+  ];
+  const allFrontEndTechnologies = [
+    'HTML/CSS',
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'Angular',
+    'Vue.js',
+    'Svelte',
+    'Next.js',
+    'Redux',
+    'Bootstrap',
+    'Tailwind CSS',
+    'SASS/LESS',
+  ];
+
+  const allBackEndTechnologies = [
+    'Node.js',
+    'Express.js',
+    'Deno',
+    'NestJS',
+    'AdonisJS',
+    'Koa.js',
+    'Sails.js',
+    'Meteor.js',
+    'Hapi.js',
+    'LoopBack',
+    'Feathers.js',
+    'Socket.io',
+  ];
+
+  const [frontEnd, setFrontEnd] = useState([]);
+  const [selectedFrontEnd, setSelectedFrontEnd] = useState('');
+  const [frontEndList, setFrontEndList] = useState(allFrontEndTechnologies);
+
+  const [backEnd, setBackEnd] = useState([]);
+  const [selectedBackEnd, setSelectedBackEnd] = useState('');
+  const [backEndList, setBackEndList] = useState(allBackEndTechnologies);
+
+  const handleChange = e => {
     const { name, value } = e.target;
-    if (name === "projectTitle") {
+    if (name === 'projectTitle') {
       setProjectTitle(value);
-    } else if (name === "projectDesc") {
+    } else if (name === 'projectDesc') {
       setProjectDesc(value);
-    } else if (name === "projectRolesNeeded") {
+    } else if (name === 'projectRolesNeeded') {
       setProjectRolesNeeded(value);
-    } else if (name.startsWith("technologies")) {
-      const [, technologyKey] = name.split(".");
-      setTechnologies((prevTechnologies) => ({
-        ...prevTechnologies,
-        [technologyKey]: value,
-      }));
-    } else {
+    } else if (name === 'selectedFrontEnd') {
       setSelectedFrontEnd(value);
+    } else if (name === 'selectedBackEnd') {
+      setSelectedBackEnd(value);
     }
   };
-  
+
+  const handleChanges = e => {
+    const { value } = e.target;
+    setSelectedRoles(prevSelectedRoles => {
+      if (prevSelectedRoles.includes(value)) {
+        return prevSelectedRoles.filter(role => role !== value);
+      } else {
+        return [...prevSelectedRoles, value];
+      }
+    });
+  };
 
   const handleAddFrontEnd = () => {
-    if (selectedFrontEnd.trim() !== "") {
-      setFrontEnd((prevFrontEnd) => [...prevFrontEnd, selectedFrontEnd]);
-      setFrontEndList((prevList) => prevList.filter((item) => item !== selectedFrontEnd));
-      setSelectedFrontEnd("");
+    if (selectedFrontEnd.trim() !== '') {
+      setFrontEnd(prevFrontEnd => [...prevFrontEnd, selectedFrontEnd]);
+      setFrontEndList(prevList => prevList.filter(item => item !== selectedFrontEnd));
+      setSelectedFrontEnd('');
     }
   };
 
-  const removeItem = (param) => {
-    setFrontEnd((prevFrontEnd) => prevFrontEnd.filter((item) => item !== param));
-    setFrontEndList((prevList) => [...prevList, param]);
+  const handleAddBackEnd = () => {
+    if (selectedBackEnd.trim() !== '') {
+      setBackEnd(prevBackEnd => [...prevBackEnd, selectedBackEnd]);
+      setBackEndList(prevList => prevList.filter(item => item !== selectedBackEnd));
+      setSelectedBackEnd('');
+    }
   };
 
-  const handleSubmit = async (e) => {
+  const removeItem = param => {
+    setFrontEnd(prevFrontEnd => prevFrontEnd.filter(item => item !== param));
+    setFrontEndList(prevList => [...prevList, param]);
+    setBackEnd(prevBackEnd => prevBackEnd.filter(item => item !== param));
+    setBackEndList(prevList => [...prevList, param]);
+  };
+
+  const handleSubmit = async e => {
     e.preventDefault();
 
     const updatedProject = {
       title: projectTitle,
       description: projectDesc,
-      rolesNeeded: projectRolesNeeded,
-      // frontEnd
+      rolesNeeded: selectedRoles,
+      technologies: {
+        frontend: frontEnd,
+        backend: backEnd,
+      },
     };
+    console.log('Data being sent to backend:', updatedProject);
 
     try {
       const response = await fetch(`http://localhost:8000/api/v1/projects/${projectId}`, {
@@ -65,12 +135,12 @@ const EditProject = ({ projectId, projectTitle: initialProjectTitle, projectDesc
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(updatedProject),
-        credentials: 'include' 
+        credentials: 'include',
       });
 
       if (response.ok) {
         navigate(`/profile`);
-        } else {
+      } else {
         throw new Error('Failed to update project');
       }
     } catch (error) {
@@ -84,44 +154,119 @@ const EditProject = ({ projectId, projectTitle: initialProjectTitle, projectDesc
       <form className="w-full h-[90%] py-6 flex flex-col" onSubmit={handleSubmit}>
         <div className="flex flex-row justify-center">
           <div className="w-[60%] flex flex-col">
-            <Input label="Project Title" name="projectTitle" className="text-gray" value={projectTitle} onChange={handleChange} />
+            <Input
+              label="Project Title"
+              name="projectTitle"
+              className="text-gray"
+              value={projectTitle}
+              onChange={handleChange}
+            />
             <div className="mt-10">
-              <Textarea label="About Project" name="projectDesc" className="text-gray" value={projectDesc} onChange={handleChange} />
+              <Textarea
+                label="About Project"
+                name="projectDesc"
+                className="text-gray"
+                value={projectDesc}
+                onChange={handleChange}
+              />
             </div>
-            <div className="mt-10 pb-10 flex flex-row bg-black">
-              <select name="selectedFrontEnd" value={selectedFrontEnd} onChange={handleChange} className="bg-black z-10 text-white outline-none">
-                <option value="">Select a technology</option>
-                {frontEndList.map((item, index) => (
-                  <option value={item} key={index}>{item}</option>
-                ))}
+            <div className="mt-10 flex flex-row bg-black">
+              <select
+                name="selectedFrontEnd"
+                value={selectedFrontEnd}
+                onChange={handleChange}
+                className="bg-black z-10 text-white outline-none"
+              >
+                <option value="">Select a frontend technology</option>
+                {frontEndList
+                  .filter(item => !frontEnd.includes(item))
+                  .map((item, index) => (
+                    <option value={item} key={index}>
+                      {item}
+                    </option>
+                  ))}
               </select>
-              <button type="button" onClick={handleAddFrontEnd} className="ml-2 px-4 py-1 bg-green text-white rounded-md">Add</button>
+              <button
+                type="button"
+                onClick={handleAddFrontEnd}
+                className="ml-2 px-4 py-1 bg-green text-black rounded-md"
+              >
+                Add
+              </button>
             </div>
-            <div>
+            <div className="flex flex-row mt-2">
               {frontEnd.map((item, index) => (
                 <div className="flex flex-row" key={index}>
                   <div>{item}</div>
-                  <XCircleIcon onClick={() => removeItem(item)} strokeWidth="1" className="h-5 w-5 stroke-blue/50 hover:stroke-blue hover:cursor-pointer" />
+                  <XCircleIcon
+                    onClick={() => removeItem(item)}
+                    strokeWidth="1"
+                    className="h-5 w-5 stroke-blue/50 hover:stroke-blue hover:cursor-pointer"
+                  />
                 </div>
               ))}
             </div>
-            <div>
-              <Textarea label="Roles needed" name="projectRolesNeeded" className="text-gray" value={projectRolesNeeded} onChange={handleChange} />
+            <div className="mt-10 flex flex-row bg-black">
+              <select
+                name="selectedBackEnd"
+                value={selectedBackEnd}
+                onChange={handleChange}
+                className="bg-black z-10 text-white outline-none"
+              >
+                <option value="">Select a backend technology</option>
+                {backEndList
+                  .filter(item => !backEnd.includes(item))
+                  .map((item, index) => (
+                    <option value={item} key={index}>
+                      {item}
+                    </option>
+                  ))}
+              </select>
+              <button
+                type="button"
+                onClick={handleAddBackEnd}
+                className="ml-2 px-4 py-1 bg-green text-black rounded-md"
+              >
+                Add
+              </button>
+            </div>
+            <div className="flex flex-row mt-2">
+              {backEnd.map((item, index) => (
+                <div className="flex flex-row" key={index}>
+                  <div>{item}</div>
+                  <XCircleIcon
+                    onClick={() => removeItem(item)}
+                    strokeWidth="1"
+                    className="h-5 w-5 stroke-blue/50 hover:stroke-blue hover:cursor-pointer"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="mt-10 pb-10 grid grid-cols-2 gap-4">
+              {allRoles.map((role, index) => (
+                <label key={index} className="flex items-center">
+                  <input
+                    type="checkbox"
+                    name={role}
+                    value={role}
+                    checked={selectedRoles.includes(role)}
+                    onChange={handleChanges}
+                    className="mr-2"
+                  />
+                  {role}
+                </label>
+              ))}
             </div>
           </div>
         </div>
         <div className="flex flex-row w-full justify-end items-end">
-          <button
-            variant="gradient"
-            color="green"
-            // onClick={""}
-            className="btn-primary text-black w-[30%] "
-          >Submit
+          <button variant="gradient" color="green" className="btn-primary text-black w-[30%] ">
+            Submit
           </button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default EditProject;
