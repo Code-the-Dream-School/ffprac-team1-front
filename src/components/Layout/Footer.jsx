0553bb0579d224c3efb6c377906951fd06eccecf
mariyador <aniknorod@gmail.com>
2024-04-28 17:16:14 -0400

    refactoring of the footer.jsx

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
-
   return (
-<footer className="bg-black p-4 flex-1 bottom-0">
-  <p className="text-center text-blue/40"> 
-    Copyright &copy; 2024 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    Designed by DevConnexion  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a href="/About">Meet the Team</a>
-  </p>
-</footer>
+    <footer className="bg-black p-4 flex-1 bottom-0">
+      <p className="text-center text-blue/40">
+        Copyright &copy; 2024 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Designed
+        by DevConnexion &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="/about">Meet the Team</a>
+      </p>
+    </footer>
   );
 };
 
