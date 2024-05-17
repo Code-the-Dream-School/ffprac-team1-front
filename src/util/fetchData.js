1794142523386d8d3ecc2085b387563cd87b58ed
Anna Solovykh <ms.teachy.solo@gmail.com>
2024-05-16 21:18:15 -0400

    add version to url

--- src/util/fetchData.js
+++ src/util/fetchData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //const API_BASE_URL = 'http://localhost:8000/api/v1';
 //const API_BASE_URL = 'https://dev-connexion-g6sv.onrender.com/api/v1';
-const API_BASE_URL = 'https://dev-connexion-pr-63.onrender.com';
+const API_BASE_URL = 'https://dev-connexion-pr-63.onrender.com/api/v1';
 
 export const register = async ({ firstName, lastName, email, password }) => {
   try {
